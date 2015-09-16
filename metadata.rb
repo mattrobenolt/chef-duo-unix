54b5dea8709182e185dc07da0d04d0043a1f6638
Matt Robenolt <matt@ydekproductions.com>
2015-09-16 12:57:44 -0700

    Fix up metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
+name             "duo-unix"
 maintainer       "Hung Truong"
 maintainer_email "hung@hung-truong.com"
 license          "Apache 2.0"
 description      "Installs/Configures Duo Unix two-factor authentication"
 version          "0.1"
 
-depends          "ssh-cookbook", ">= 1.4.0"
+depends          "ssh", ">= 1.4.0"
